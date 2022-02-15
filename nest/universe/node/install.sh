ceb91fe5311c31229930314ae01813290d6abfa6
Samuel J Voeller <12182390+xCykrix@users.noreply.github.com>
2022-02-15 03:21:10 +0100

    feat: add updates yolk and scripts

--- nest/universe/node/install.sh
+++ nest/universe/node/install.sh
@@ -1,76 +1,91 @@
 #!/bin/bash
 
-echo "[Pterodactyl:install.sh] Node.js Universal :: STEP_INSTALL"
-echo ": MKDIR /mnt/server/" && mkdir -p /mnt/server/ > /dev/null
-echo ": ENTDIR /mnt/server/" && cd /mnt/server/ > /dev/null || exit
-
-if [ "${UPDATE}" == "false" ] || [ "${UPDATE}" == "0" ]; then
-  echo ""
-  echo "WARN: Update via Git -> Disabled by user startup configuration."
-  echo "  + You are required to manually upload your project, excluding node_modules, unless enabled."
-  echo "  + If you would like to enable this feature, please update your startup settings and reinstall your server."
-  echo ""
-  echo ": EXIT [0] / The installation has been successfully completed."
+# Define the exit function.
+success()
+          {
+  echo
+  echo 'The application has completed installation successfully. Please review the transaction log for any issue then start the service.'
   exit 0
+}
+
+# Define the error function.
+error()
+        {
+  echo
+  echo 'The installation process failed due to an error. The reference code and error message are attached below.'
+  echo
+  echo "Reference Code: ${1}"
+  echo "${2}"
+  echo
+  echo 'Please correct the above mentioned issues, or reach out to support.'
+  echo 'Want to try again? Navigate to "Settings" and select "Reinstall Server" to attempt the install process once more.'
+  exit 254
+}
+
+# Print the initial header.
+echo '[amethyst-studio/pterodactyl-nest] Preparing to install the Node.js-configured environment.'
+echo '  Copyright (c) 2021 Samuel Voeller on behalf of Amethyst [Live] Studio.'
+echo '  Find the source at https://github.com/amethyst-studio/pterodactyl-nest'
+
+# Check if the updater is to be used. This will rapid-terminate the installation for self-service mode.
+if [[ ${UPDATE} == 'false' ]] || [[ ${UPDATE} == '0' ]]; then
+  echo 'Skipping the git-based operations due to the updater variable being set to "false" or "0".'
+  success
 fi
 
+# Prepare the installation environment.
+echo
+echo '$ mkdir /mnt/server/' && mkdir -p /mnt/server
+echo '$ cd /mnt/server/' && cd /mnt/server || exit 254
+
+# Ensure the remote is correctly formatted.
 if [[ ${GIT_REMOTE} != *.git ]]; then
-  GIT_REMOTE=${GIT_REMOTE}.git
+  GIT_REMOTE="${GIT_REMOTE}.git"
 fi
 
-if [ -z "${GIT_USERNAME}" ] && [ -z "${GIT_ACCESS_TOKEN}" ]; then
-  echo "ERR: Update via Git -> Detected invalid user startup configuration."
-  echo "  + You must specify both a valid username and access token to use this feature."
-  echo ""
-  echo ": EXIT [1] / The installation has failed. Please review the console for more information."
-  echo ": EXIT [1] / Navigate to 'Settings' and select 'Reinstall Server' once resolved."
-  exit 1
+# Ensure the environment variables are defined.
+if [[ -z ${GIT_REMOTE} ]] || [[ -z ${GIT_USERNAME} ]] || [[ -z ${GIT_ACCESS_TOKEN} ]]; then
+  error \
+    'ERR_ENV_VARIABLE_MISSING' \
+    'Ensure that the GIT_REMOTE, GIT_USERNAME, and GIT_ACCESS_TOKEN environment variables are set under the "Startup" view. These are all required for automatic updates.'
 else
-  GIT_REMOTE=https://${GIT_USERNAME}:${GIT_ACCESS_TOKEN}@$(echo -e "${GIT_REMOTE}" | cut -d/ -f3-)
+  GIT_REMOTE="https://${GIT_USERNAME}:${GIT_ACCESS_TOKEN}@$(echo -e "${GIT_REMOTE}" | cut -d/ -f3-)"
 fi
 
-if [ "$(ls -A /mnt/server)" ]; then
-  echo ": CHKDIR /mnt/server/ EXISTS"
-  if [ -d .git ]; then
-    echo ": CHKDIR /mnt/server/.git/ EXISTS"
-    if [ -f .git/config ]; then
-      echo -e ": CHKFILE /mnt/server/.git/config"
+# Detect if the .git directory is available to update vs using a fresh clone.
+LS_MNT_SERVER=$(ls -A .)
+if [[ -n ${LS_MNT_SERVER} ]]; then
+  echo 'Detected files within '/mnt/server' and is now attempting to use the existing project.'
+  if [[ -d .git ]]; then
+    echo 'Locating the persistent origin of the current project...'
+    if [[ -f .git/config ]]; then
       ORIGIN=$(git config --get remote.origin.url)
+      echo 'Remote is '"${ORIGIN}"''
     else
-      echo "ERR: Update via Git -> Detected invalid '/mnt/server/.git/config'."
-      echo "  + The origin of the .git directory does not match the configured Git Remote URL."
-      echo ""
-      echo ": EXIT [1] / The installation has failed. Please review the console for more information."
-      echo ": EXIT [1] / Navigate to 'Settings' and select 'Reinstall Server' once resolved."
-      exit 1
+      error \
+        'GIT_CONFIG_MISSING_OR_CORRUPT' \
+        'Please test the '.git' folder integrity. It potentially is missing files or corrupted in some form.'
     fi
-  fi
-
-  if [ "${ORIGIN}" == "${GIT_REMOTE}" ]; then
-    echo ": COMMAND -> git fetch --all"
-    git fetch --all > /dev/null
-    if [ -f package-lock.json ]; then
-      echo ": COMMAND -> rm -f package-lock.json"
-      rm -f package-lock.json > /dev/null
-    fi
-    echo ": COMMAND -> git reset --hard"
-    git reset --hard > /dev/null
-    echo ": COMMAND -> git pull"
-    git pull > /dev/null
-  fi
-else
-  echo ": CHKDIR /mnt/server/ NOT-EXISTS"
-  if [ -z "${GIT_BRANCH}" ]; then
-    git config --global user.email "ptero@mxsrv.amethyst.live" > /dev/null
-    git config --global user.name "Pterodactyl Panel Automation" > /dev/null
-    echo -e ": COMMAND -> git clone \"${GIT_REMOTE}\" ."
-    git clone "${GIT_REMOTE}" .
-  else
-    git config --global user.email "ptero@mxsrv.amethyst.live" > /dev/null
-    git config --global user.name "Pterodactyl Panel Automation" > /dev/null
-    echo -e ": COMMAND -> git clone --single-branch --branch \"${GIT_BRANCH}\" \"${GIT_ADDRESS}\" ."
-    git clone --single-branch --branch "${GIT_BRANCH}" "${GIT_ADDRESS}" .
   fi
 fi
-echo ": EXIT [0] / The installation has been successfully completed."
-exit 0
+
+# Set the global git configuration.
+echo
+echo '$ git config --global user.email report@amethyst.live' && git config --global user.email 'report@amethyst.live'
+echo '$ git config --global user.name Amethyst Pterodactyl.io' && git config --global user.name 'Amethyst Pterodactyl.io'
+
+# Determine the installation method.
+if [[ ${ORIGIN} != '' ]]; then
+  # Force the project to sync with the origin being available.
+  echo
+  echo '$ git remote set-url origin GIT_REMOTE' && git remote set-url origin "${GIT_REMOTE}" > /dev/null 2>&1
+  echo '$ git fetch --all' && git fetch --all
+  echo '$ git reset --hard' && git reset --hard
+  echo '$ git pull' && git pull
+  success
+else
+  # Attempt to clone with GIT_REMOTE.
+  echo
+  echo "$ git clone --single-branch --branch ${GIT_BRANCH} ${GIT_REMOTE} ." && git clone --single-branch --branch "${GIT_BRANCH}" "${GIT_REMOTE}" .
+  success
+fi
--- nest/universe/node/startup.sh
+++ nest/universe/node/startup.sh
@@ -1,22 +1,25 @@
-if [ -d .git ] && [ "${UPDATE}" == "1" ]; then
-  git config --global pull.rebase true
-  git fetch --all > /dev/null
-  if [ -f package-lock.json ]; then
-    rm -f package-lock.json > /dev/null
-  fi
-  git reset --hard > /dev/null
-  git pull
+#!/bin/bash
+
+# Fetch the latest source code.
+if [[ -d .git ]] && [[ ${UPDATE} == '1' ]]; then
+  echo "$ git config --global pull.rebase true" && git config --global pull.rebase true
+  echo "$ git fetch --all" && git fetch --all
+  echo "$ git reset --hard" && git reset --hard
+  echo "$ git pull" && git pull
+  echo "$ git ls-files --error-unmatch package-lock.json || rm -f ./package-lock.json"
+  git ls-files --error-unmatch package-lock.json || rm -f package-lock.json
 fi
 
-echo "Install: node-gyp"
-npm install node-gyp > /dev/null
+# Build the extra binaries used to install certain packages.
+echo "$ npm install --no-save node-gyp" && npm install --no-save node-gyp > /dev/null 2>&1
+echo "$ npm install --no-save @mapbox/node-pre-gyp" && npm install --no-save @mapbox/node-pre-gyp > /dev/null 2>&1
 
-echo "Install: package.json"
-npm install
+# Install the latest application software based on the lock file if it is tracked.
+echo "$ npm install --only=production" && npm install --only=production > /dev/null 2>&1
 
-echo -e "Launch: ${SCRIPT}"
-if [ "${RESTART}" == "true" ] || [ "${RESTART}" == "1" ]; then
-  forever --minUptime=500 --spinSleepTime=1000 -c "node --max-old-space-size={{SERVER_MEMORY}}" "{{SCRIPT}}"
+# Launch the application via the specified mode.
+if [[ ${RESTART} == 'true' ]] || [[ ${RESTART} == '1' ]]; then
+  forever --minUptime=500 --spinSleepTime=1000 -c 'node --max-old-space-size={{SERVER_MEMORY}} {{SCRIPT}}'
 else
-  node --max-old-space-size="{{SERVER_MEMORY}}" "{{SCRIPT}}"
+  node --max-old-space-size='{{SERVER_MEMORY}}' '{{SCRIPT}}'
 fi
