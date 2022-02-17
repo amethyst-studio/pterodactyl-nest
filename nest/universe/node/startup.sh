5a351364e37cbb9dacfe8a9facc58cb6ff6d8edd
Samuel J Voeller <12182390+xCykrix@users.noreply.github.com>
2022-02-17 16:18:09 +0100

    feat: add updated logging

--- nest/universe/node/startup.sh
+++ nest/universe/node/startup.sh
@@ -2,6 +2,7 @@
 
 # Fetch the latest source code.
 if [[ -d .git ]] && [[ ${UPDATE} == '1' ]]; then
+  echo "Updating the remote sources from the remote Git repository."
   echo "$ git config --global pull.rebase true" && git config --global pull.rebase true
   echo "$ git fetch --all" && git fetch --all
   echo "$ git reset --hard" && git reset --hard
@@ -12,22 +13,26 @@ fi
 
 # Build the extra binaries used to install certain packages.
 if [[ ${INSTALL_CORE} == '1' ]]; then
-  echo "Installing core packages used for building other special packages."
+  echo "Installing core packages as requested by the environment."
   echo "$ npm install --no-save node-gyp" && npm install --no-save node-gyp > /dev/null 2>&1
   echo "$ npm install --no-save @mapbox/node-pre-gyp" && npm install --no-save @mapbox/node-pre-gyp > /dev/null 2>&1
 fi
 
 # Install additional packages used by the main npm install process.
 if [[ ${INSTALL_ADDITIONAL} != ""   ]]; then
+  echo "Installing additional packages as requested by the environment."
   echo "$ npm install --no-save ${INSTALL_ADDITIONAL}" && npm install --no-save "${INSTALL_ADDITIONAL}" > /dev/null 2>&1
 fi
 
 # Install the latest application software based on the lock file if it is tracked.
+echo "Installing the production dependencies from the 'package.json' provided by the user."
 echo "$ npm install --only=production" && npm install --only=production > /dev/null 2>&1
 
 # Launch the application via the specified mode.
 if [[ ${AUTOMATIC_RESTART} == '1' ]]; then
+  echo "Launching the application in fast-restart mode."
   npx --yes --package=forever -c forever --minUptime=500 --spinSleepTime=1000 -c 'node --max-old-space-size={{SERVER_MEMORY}} {{SCRIPT}}'
 else
+  echo "Launching the application in full-restart mode."
   node --max-old-space-size='{{SERVER_MEMORY}}' '{{SCRIPT}}'
 fi
